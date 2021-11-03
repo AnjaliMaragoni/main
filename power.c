ae02fe909e4ce4e6f7ac053aa3341243a5a8975c
Anjali Maragoni <83865472+AnjaliMaragoni@users.noreply.github.com>
2021-11-03 18:52:18 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+   int base,exp,res=1;
+   scanf("%d %d",&base,&exp);
+  
+   while(exp!=0)
+   {
+       res=res*base;
+       exp--;
+   }
+   printf("%d",res);
+   return 0;
+}  
